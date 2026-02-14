eddb2685c3828f3381a76d3f0a3434d30301f452
Monarch-Codes00 <chinedumroster@gmail.com>
2026-02-14 13:33:13 +0930

    feat: Add LeaderboardController to expose an eco leaderboard endpoint.

---
+++ SpringBackend/src/main/java/com/nexuslogistics/controller/LeaderboardController.java
@@ -0,0 +1,23 @@
+package com.nexuslogistics.controller;
+
+import com.nexuslogistics.model.DriverScore;
+import com.nexuslogistics.repository.DriverScoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/leaderboard")
+public class LeaderboardController {
+
+    @Autowired
+    private DriverScoreRepository driverScoreRepository;
+
+    @GetMapping("/eco")
+    public List<DriverScore> getEcoLeaderboard() {
+        return driverScoreRepository.findAllByOrderByEcoScoreDesc();
+    }
+}

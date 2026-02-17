7a1a81266fd9deafef83ad56c32bef2212153264
Monarch-Codes00 <chinedumroster@gmail.com>
2026-02-17 12:21:26 +0930

    feat: add TelemetryController for real-time vehicle telemetry updates and DocumentController for waybill generation.

--- SpringBackend/src/main/java/com/nexuslogistics/controller/DispatchController.java
+++ SpringBackend/src/main/java/com/nexuslogistics/controller/DispatchController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/dispatch")
+@CrossOrigin(origins = "*")
 public class DispatchController {
 
     @Autowired
--- SpringBackend/src/main/java/com/nexuslogistics/controller/DocumentController.java
+++ SpringBackend/src/main/java/com/nexuslogistics/controller/DocumentController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/documents")
+@CrossOrigin(origins = "*")
 public class DocumentController {
 
     @Autowired
